eff8715d0c6ba78c029c7da072c813e79623ab97
Satchapum Krailertrat <satchapum.mail@mail.kmutt.ac.th>
2022-11-21 16:00:54 +0700

    add Calculatetime method

--- CalculateTime.cs
+++ CalculateTime.cs
@@ -1,3 +1,84 @@
 class CalculateTime {
+    private Queue<string> BM_BKT07_30 = new Queue<string>();
+    private Queue<string> BM_BKT08_00 = new Queue<string>();
+    private Queue<string> BM_BKT08_15 = new Queue<string>();
+    private Queue<string> BM_BKT08_30 = new Queue<string>();
+    private Queue<string> BM_BKT12_00 = new Queue<string>();
+    private Queue<string> BM_BKT14_00 = new Queue<string>();
+    private Queue<string> BM_BKT16_00 = new Queue<string>();
+
+    private Queue<string> BKT_BM07_00 = new Queue<string>();
+    private Queue<string> BKT_BM10_00 = new Queue<string>();
+    private Queue<string> BKT_BM13_00 = new Queue<string>();
+    private Queue<string> BKT_BM15_00 = new Queue<string>();
+    private Queue<string> BKT_BM16_30 = new Queue<string>();
+    private Queue<string> BKT_BM17_00 = new Queue<string>();
+    private Queue<string> BKT_BM17_30 = new Queue<string>();
+    private Queue<string> BKT_BM18_30 = new Queue<string>();
+
+    public CalculateTime(Queue<string> BM_BKT07_30, Queue<string> BM_BKT08_00, Queue<string> BM_BKT08_15, Queue<string> BM_BKT08_30, Queue<string> BM_BKT12_00, Queue<string> BM_BKT14_00
+    ,Queue<string> BM_BKT16_00, Queue<string> BKT_BM07_00, Queue<string> BKT_BM10_00, Queue<string> BKT_BM13_00, Queue<string> BKT_BM15_00, Queue<string> BKT_BM16_30, Queue<string> BKT_BM17_00
+    , Queue<string> BKT_BM17_30, Queue<string> BKT_BM18_30) 
+    {
+        this.BM_BKT07_30 = BM_BKT07_30;
+        this.BM_BKT08_00 = BM_BKT08_00; 
+        this.BM_BKT08_15 = BM_BKT08_15; 
+        this.BM_BKT08_30 = BM_BKT08_30; 
+        this.BM_BKT12_00 = BM_BKT12_00;  
+        this.BM_BKT14_00 = BM_BKT14_00; 
+        this.BM_BKT16_00 = BM_BKT16_00; 
+
+        this.BKT_BM07_00 = BKT_BM07_00;
+        this.BKT_BM10_00 = BKT_BM10_00; 
+        this.BKT_BM13_00 = BKT_BM13_00; 
+        this.BKT_BM15_00 = BKT_BM15_00; 
+        this.BKT_BM16_30 = BKT_BM16_30; 
+        this.BKT_BM17_00 = BKT_BM17_00; 
+        this.BKT_BM17_30 = BKT_BM17_30;
+        this.BKT_BM18_30 = BKT_BM18_30;    
+    }
+    public Queue<string> GetBM_BKT07_30() {
+        return this.BM_BKT07_30;
+    }
+    public Queue<string> GetBM_BKT08_00() {
+        return this.BM_BKT08_00;
+    }
+    public Queue<string> GetBM_BKT08_15() {
+        return this.BM_BKT08_15;
+    }
+    public Queue<string> GetBM_BKT08_30() {
+        return this.BM_BKT08_30;
+    }
+    public Queue<string> GetBM_BKT12_00() {
+        return this.BM_BKT12_00;
+    }
+    public Queue<string> GetBM_BKT14_00() {
+        return this.BM_BKT14_00;
+    }
+    public Queue<string> GetBM_BKT16_00() {
+        return this.BM_BKT16_00;
+    }
+    public Queue<string> GetBKT_BM07_00() {
+        return this.BKT_BM07_00;
+    }
+    public Queue<string> GetBKT_BM10_00() {
+        return this.BKT_BM10_00;
+    }
+    public Queue<string> GetBKT_BM13_00() {
+        return this.BKT_BM13_00;
+    }
+    public Queue<string> GetBKT_BM15_00() {
+        return this.BKT_BM15_00;
+    }
+    public Queue<string> GetBKT_BM16_30() {
+        return this.BKT_BM16_30;
+    }
+    public Queue<string> GetBKT_BM17_00() {
+        return this.BKT_BM17_00;
+    }
+    public Queue<string> GetBKT_BM18_30() {
+        return this.BKT_BM18_30;
+    }
     
+
 }
--- Program.cs
+++ Program.cs
@@ -6,87 +6,149 @@ enum MenuII {
 }
 class Program {
     static RegisterInfoList registerList;
-    public static void DeclareQueue() {
-        Queue<string> BM_BKT07_30 = new Queue<string>();
-        Queue<string> BM_BKT08_00 = new Queue<string>();
-        Queue<string> BM_BKT08_15 = new Queue<string>();
-        Queue<string> BM_BKT08_30 = new Queue<string>();
-        Queue<string> BM_BKT12_00 = new Queue<string>();
-        Queue<string> BM_BKT14_00 = new Queue<string>();
-        Queue<string> BM_BKT16_00 = new Queue<string>();
-
-        Queue<string> BKT_BM07_00 = new Queue<string>();
-        Queue<string> BKT_BM10_00 = new Queue<string>();
-        Queue<string> BKT_BM13_00 = new Queue<string>();
-        Queue<string> BKT_BM15_00 = new Queue<string>();
-        Queue<string> BKT_BM16_30 = new Queue<string>();
-        Queue<string> BKT_BM17_00 = new Queue<string>();
-        Queue<string> BKT_BM17_30 = new Queue<string>();
-        Queue<string> BKT_BM18_30 = new Queue<string>();
-    }
     public static void main(string[] args) {
-        
-        DeclareQueue();
+
+    Queue<string> BM_BKT07_30 = new Queue<string>();
+    Queue<string> BM_BKT08_00 = new Queue<string>();
+    Queue<string> BM_BKT08_15 = new Queue<string>();
+    Queue<string> BM_BKT08_30 = new Queue<string>();
+    Queue<string> BM_BKT12_00 = new Queue<string>();
+    Queue<string> BM_BKT14_00 = new Queue<string>();
+    Queue<string> BM_BKT16_00 = new Queue<string>();
+
+    Queue<string> BKT_BM07_00 = new Queue<string>();
+    Queue<string> BKT_BM10_00 = new Queue<string>();
+    Queue<string> BKT_BM13_00 = new Queue<string>();
+    Queue<string> BKT_BM15_00 = new Queue<string>();
+    Queue<string> BKT_BM16_30 = new Queue<string>();
+    Queue<string> BKT_BM17_00 = new Queue<string>();
+    Queue<string> BKT_BM17_30 = new Queue<string>();
+    Queue<string> BKT_BM18_30 = new Queue<string>();
+
         while(true) {
+
             string text = DateTime.Now.ToLongTimeString();
             Console.WriteLine(text);
             Thread.Sleep(1000);//เรียกต่อ1วินาที
 
-            if(text == "07:00:00" ) { //บางขุน
-                Console.WriteLine("Time Table : 07:00:00");
-                for(int Number = 0; Number <= BM_BKT07_30.GetLength()-1; Number++) {
-                    Console.WriteLine("{0}. {1}", Number+1, BM_BKT07_30.Get(Number));
-                }
-            }
-            else if(text == "07:30:00") {
-                
-            }
-            else if(text == "08:00:00") {
-                
-            }
-            else if(text == "08:15:00") {
-                
-            }
-            else if(text == "08:30:00") {
-                
-            }
-            else if(text == "07:00:00") { //บางขุน
-                
-            }
-            else if(text == "10:00:00") { //บางขุน
-                
-            }
-            else if(text == "12:00:00") {
-                
-            }
-            else if(text == "13:00:00") { //บางขุน
-                
-            }
-            else if(text == "14:00:00") {
-                
-            }
-            else if(text == "15:00:00") { //บางขุน
-                
-            }
-            else if(text == "16:00:00") {
-                
-            }
-            else if(text == "16:30:00") { //บางขุน
-                
-            }
-            else if(text == "17:00:00") { //บางขุน
-                
-            }
-            else if(text == "17:30:00") { //บางขุน
-                
-            }
-            else if(text == "18:30:00") { //บางขุน
-                
-            }
+            CalculateTime(text, BM_BKT07_30, BM_BKT08_00, BM_BKT08_15, BM_BKT08_30, BM_BKT12_00, BM_BKT14_00
+            , BM_BKT16_00, BKT_BM07_00, BKT_BM10_00, BKT_BM13_00, BKT_BM15_00, BKT_BM16_30, BKT_BM17_00
+            , BKT_BM17_30, BKT_BM18_30);
             PrepareRegisterListWhenProgramIsLoad();
             PrintMenuScreen();
+            
         }
         
+    }
+    public static void CalculateTime(string text, Queue<string> BM_BKT07_30, Queue<string> BM_BKT08_00, Queue<string> BM_BKT08_15, Queue<string> BM_BKT08_30, Queue<string> BM_BKT12_00
+    , Queue<string> BM_BKT14_00, Queue<string> BM_BKT16_00, Queue<string> BKT_BM07_00, Queue<string> BKT_BM10_00, Queue<string> BKT_BM13_00, Queue<string> BKT_BM15_00, Queue<string> BKT_BM16_30
+    , Queue<string> BKT_BM17_00, Queue<string> BKT_BM17_30, Queue<string> BKT_BM18_30) {
+        
+        if(text == "07:00:00" ) { //บางขุน
+            Console.WriteLine("Time Table : 07:00:00");
+            for(int Number = 0; Number <= BKT_BM07_00.GetLength()-1; Number++) {
+                Console.WriteLine("{0}. {1}", Number+1, BKT_BM07_00.Get(Number));
+                BKT_BM07_00.Pop();
+            }
+        }
+        else if(text == "07:30:00") {
+            Console.WriteLine("*************| Time Table : 07:30:00 |*************");
+            for(int Number = 0; Number <= BM_BKT07_30.GetLength()-1; Number++) {
+                Console.WriteLine("{0}. {1}", Number+1, BM_BKT07_30.Get(Number));
+                BM_BKT07_30.Pop();
+            }
+        }
+        else if(text == "08:00:00") {
+            Console.WriteLine("*************| Time Table : 08:00:00 |*************");
+            for(int Number = 0; Number <= BM_BKT08_00.GetLength()-1; Number++) {
+                Console.WriteLine("{0}. {1}", Number+1, BM_BKT08_00.Get(Number));
+                BM_BKT08_00.Pop();
+            }
+        }
+        else if(text == "08:15:00") {
+            Console.WriteLine("*************| Time Table : 08:15:00 |*************");
+            for(int Number = 0; Number <= BM_BKT08_15.GetLength()-1; Number++) {
+                Console.WriteLine("{0}. {1}", Number+1, BM_BKT08_15.Get(Number));
+                BM_BKT08_15.Pop();
+            }
+        }
+        else if(text == "08:30:00") {
+            Console.WriteLine("*************| Time Table : 08:30:00 |*************");
+            for(int Number = 0; Number <= BM_BKT08_30.GetLength()-1; Number++) {
+                Console.WriteLine("{0}. {1}", Number+1, BM_BKT08_30.Get(Number));
+                BM_BKT08_30.Pop();
+            }
+        }
+        else if(text == "10:00:00") { //บางขุน
+            Console.WriteLine("*************| Time Table : 10:00:00 |*************");
+            for(int Number = 0; Number <= BKT_BM10_00.GetLength()-1; Number++) {
+                Console.WriteLine("{0}. {1}", Number+1, BKT_BM10_00.Get(Number));
+                BKT_BM10_00.Pop();
+            }
+        }
+        else if(text == "12:00:00") {
+            Console.WriteLine("*************| Time Table : 12:00:00 |*************");
+            for(int Number = 0; Number <= BM_BKT12_00.GetLength()-1; Number++) {
+                Console.WriteLine("{0}. {1}", Number+1, BM_BKT12_00.Get(Number));
+                BM_BKT12_00.Pop();
+            }
+        }
+        else if(text == "13:00:00") { //บางขุน
+            Console.WriteLine("*************| Time Table : 13:00:00 |*************");
+            for(int Number = 0; Number <= BKT_BM13_00.GetLength()-1; Number++) {
+                Console.WriteLine("{0}. {1}", Number+1, BKT_BM13_00.Get(Number));
+                BKT_BM13_00.Pop();
+            }
+        }
+        else if(text == "14:00:00") {
+            Console.WriteLine("*************| Time Table : 14:00:00 |*************");
+            for(int Number = 0; Number <= BM_BKT14_00.GetLength()-1; Number++) {
+                Console.WriteLine("{0}. {1}", Number+1, BM_BKT14_00.Get(Number));
+                BM_BKT14_00.Pop();
+            }
+        }
+        else if(text == "15:00:00") { //บางขุน
+            Console.WriteLine("*************| Time Table : 15:00:00 |*************");
+            for(int Number = 0; Number <= BKT_BM15_00.GetLength()-1; Number++) {
+                Console.WriteLine("{0}. {1}", Number+1, BKT_BM15_00.Get(Number));
+                BKT_BM15_00.Pop();
+            }
+        }
+        else if(text == "16:00:00") {
+            Console.WriteLine("*************| Time Table : 16:00:00 |*************");
+            for(int Number = 0; Number <= BM_BKT16_00.GetLength()-1; Number++) {
+                Console.WriteLine("{0}. {1}", Number+1, BM_BKT16_00.Get(Number));
+                BM_BKT16_00.Pop();
+            }
+        }
+        else if(text == "16:30:00") { //บางขุน
+            Console.WriteLine("*************| Time Table : 16:30:00 |*************");
+            for(int Number = 0; Number <= BKT_BM16_30.GetLength()-1; Number++) {
+                Console.WriteLine("{0}. {1}", Number+1, BKT_BM16_30.Get(Number));
+                BKT_BM16_30.Pop();
+                }
+        }
+        else if(text == "17:00:00") { //บางขุน
+            Console.WriteLine("*************| Time Table : 17:00:00 |*************");
+            for(int Number = 0; Number <= BKT_BM17_00.GetLength()-1; Number++) {
+                Console.WriteLine("{0}. {1}", Number+1, BKT_BM17_00.Get(Number));
+                BKT_BM17_00.Pop();
+            }
+        }
+        else if(text == "17:30:00") { //บางขุน
+            Console.WriteLine("*************| Time Table : 17:30:00 |*************");
+            for(int Number = 0; Number <= BKT_BM17_30.GetLength()-1; Number++) {
+                Console.WriteLine("{0}. {1}", Number+1, BKT_BM17_30.Get(Number));
+                BKT_BM17_30.Pop();
+            }
+        }
+        else if(text == "18:30:00") { //บางขุน
+            Console.WriteLine("*************| Time Table : 18:30:00 |*************");
+            for(int Number = 0; Number <= BKT_BM18_30.GetLength()-1; Number++) {
+                Console.WriteLine("{0}. {1}", Number+1, BKT_BM18_30.Get(Number));
+                BKT_BM18_30.Pop();
+            }
+        }
     }
     static void PrintMenuScreen()
     {
