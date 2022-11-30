e0422ff7185012a752228389b8112aff22b60c5a
taoyngwie <taoguitardz@gmil.com>
2022-11-30 12:00:44 +0700

    AddTest

--- Test_Hansome/ProgramTest.cs
+++ Test_Hansome/ProgramTest.cs
@@ -2,10 +2,50 @@ namespace Test_Hansome;
 
 public class ProgramTest
 {
+    Info mockInfoProgram = new Info("Mr.", "Satchapum", "Krailertrat");
+
     Program program = new Program();
     [Fact]
-    public void TestPrintNotification() {
+    public void TestPrintNotification()
+    {
         bool IsTrueTime = false;
     }
+
+    [Fact]
+    public void ShowTimeBM_BKT_07_30()
+    {
+
+        Assert.Equal(Program.ShowTimeBM_BKT(), "07:30");
+    }
+
+    [Fact]
+    public void ShowTimeBM_BKT_13_30()
+    {
+
+        Assert.Equal(Program.ShowTimeBM_BKT(), "13:30");
+    }
+
+    [Fact]
+    public void showTimeBKT_BM_10_00()
+    {
+
+        Assert.Equal(Program.showTimeBKT_BM(), "10:00");
+    }
+
+    [Fact]
+    public void InputTitleMr_()
+    {
+
+        Assert.Equal(Program.InputTitle(), "Mr.");
+    }
+
+    [Fact]
+    public void InputTitleMr_()
+    {
+
+        Assert.Equal(Program.InputTitle(), "Mrs.");
+    }
+
+
 
 }
